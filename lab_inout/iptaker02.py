5478f0945425520fc7c8d7581ca051d9d44c89c6
Carlos Orellana <carlos.e.orellana@gmail.com>
2019-07-22 15:45:02 -0400

    update corella1

---
+++ lab_inout/iptaker02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+user_input = input("Please enter an IPv4 address:")
+
+
+print("You told me the IPv4 address is:", user_input)
+
+vendor_name = input("Insert vendor name:")
+
+print("Vendor name provide:", vendor_name)
