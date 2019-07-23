e7789820456a7fae7c2e103acfb67ce93bc43176
Carlos Orellana <carlos.e.orellana@gmail.com>
2019-07-23 11:45:18 -0400

    update corella1

---
+++ strings/stringmethod01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+lil_string = "Alta3 Rsearch offers classes on Python coding"
+
+newlist = lil_string.split(" ")
+
+print(newlist)
+
+myiplist = ["192", "168", "0", "12"]
+singleip = ".".join(myiplist)
+
+print(singleip)
+
---
+++ strings/stringmethod02.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Alta3 Research || Author: RZFeeser@alta3.com"""
+
+def main():
+    """ Run-time code"""
+    lilstring = "Alta3 Research offers classes on Python coding"
+    newlist = lilstring.split(" ")
+    print(newlist)
+
+    myiplist = ["192", "168", "0", "12"]
+    singleip = ".".join(myiplist)
+    print(singleip)
+
+main()
+
